4304d6a811402b44bc9779a7512928b1e80e1fb7
zzjjbb <31069326+zzjjbb@users.noreply.github.com>
2026-07-28 02:29:47 -0500

    Implemented ManagedArrayPool
    - ManagedArrayPool simply adds a wrapper for ArrayPool.allocator() and gets the wrapped ManagedObj;
    - ManagedObj automatically recycles the underlying array based on native ref count mechanism using weakref.finalize()

--- ssnp_pkg/src/ssnp/utils/array_pool.py
+++ ssnp_pkg/src/ssnp/utils/array_pool.py
@@ -1,13 +1,20 @@
 import logging
+import weakref
+import wrapt
 from collections.abc import Callable
-from typing import TypeVar, Any
+from typing import TypeVar, Any, Union, Literal
+from wrapt import BaseObjectProxy
 
 from .utils import param_check
 
-T = TypeVar('T')
 
-class ArrayPool:
-    def __init__(self, prototype, like_allocator: Callable[[T], T], unique_id: Callable[[T], Any] = id):
+class ArrayPool[T]:
+    _pool: list[T]
+    allocated_count: int
+
+    def __init__(
+        self, prototype: T, like_allocator: Callable[[T], T], unique_id: Callable[[T], Any] = id
+    ):
         self._pool = []
         self._id_record = set()
         self._get_id = unique_id
@@ -27,7 +34,7 @@ class ArrayPool:
 
     def recycle(self, arr):
         # sanity check
-        param_check(prototype=self.prototype, recycled=arr)
+        param_check(prototype=self.prototype, recycled=arr, _expected_type=type(self.prototype))
         if self.prototype.dtype != arr.dtype:
             raise ValueError(f"recycled array dtype {arr.dtype} is different from "
                              f"the prototype dtype {self.prototype.dtype}")
@@ -36,3 +43,76 @@ class ArrayPool:
         # check passed
         self._pool.append(arr)
         self._id_record.add(self._get_id(arr))
+
+
+class ProxyReferenceError(RuntimeError):
+    pass
+
+
+class ManagedObj(BaseObjectProxy):
+    class Disposed:
+        def __raise_err(*_, **__):
+            raise ProxyReferenceError(f"The underlying object of ManagedObj no longer exists")
+
+        def __wrapped_setattr_fixups__(self):
+            pass
+
+        def __repr__(self):
+            return "<disposed>"
+
+        __getattr__ = __raise_err
+
+    DISPOSED = Disposed()
+    _self_finalizer: weakref.finalize
+
+    def __init__(self, obj, pool: Union[ArrayPool, weakref.ReferenceType[ArrayPool]],
+                 method_wrapper: Literal['chainable', 'descriptor', None] = None):
+        def recycle_obj():
+            # If pool is alive, re-wrap obj as a new proxy and let pool recycle it
+            if (p := pool_ref()) is not None:
+                p.recycle(cls(obj, p, method_wrapper))
+
+        super().__init__(obj)
+        cls = type(self)
+        pool_ref = pool if isinstance(pool, weakref.ReferenceType) else weakref.ref(pool)
+        self._self_finalizer = weakref.finalize(self, recycle_obj)
+        self._self_finalizer.atexit = False
+        self._self_method_wrapper = method_wrapper
+
+    def __getattr__(self, item):
+        attr = super().__getattr__(item)
+        match self._self_method_wrapper:
+            case 'chainable':
+                if callable(attr):
+                    @wrapt.decorator
+                    def intercept_return(wrapped, _, args, kwargs):
+                        result = wrapped(*args, **kwargs)
+                        if result is self.__wrapped__:
+                            return self
+                        return result
+
+                    return intercept_return(attr)
+            case 'descriptor':
+                if (getter := getattr(getattr(type(self.__wrapped__), item, None), '__get__', None)) is not None:
+                    return getter(self, type(self.__wrapped__))
+            case None:
+                pass
+            case unknown:
+                raise ValueError(f"Unknown method wrapper {unknown}")
+        # fallback for all other cases
+        return attr
+
+    def dispose(self):
+        self._self_finalizer()
+        self.__wrapped__ = self.DISPOSED
+
+    def __repr__(self):
+        return f"{type(self).__qualname__}({self.__wrapped__!r})"
+
+
+class ManagedArrayPool(ArrayPool):
+    def __init__(self, *args, method_wrapper: Literal['chainable', 'descriptor', None] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        raw_allocator = self.allocator
+        self_wr = weakref.ref(self)
+        self.allocator = lambda x: ManagedObj(raw_allocator(x), self_wr, method_wrapper)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,8 @@ import warnings
 
 import numpy as np
 
-from ssnp.utils import Config
+from ssnp.utils.utils import Config
+from ssnp.utils.array_pool import ManagedObj, ManagedArrayPool
 
 
 class TestConfig(TestCase):
@@ -39,3 +40,135 @@ class TestConfig(TestCase):
             config.xyz = new_value
             self.assertTupleEqual(config.xyz, (4, 5, 6))
             config.xyz = (3, 4, 5)
+
+
+class MockArray:
+    id_counter = 0
+    freed_obj = 0
+
+    @classmethod
+    def get_unique_id(cls):
+        cls.id_counter += 1
+        return cls.id_counter
+
+    @classmethod
+    def bump_freed(cls):
+        cls.freed_obj += 1
+
+    @classmethod
+    def clear_counters(cls):
+        cls.id_counter = cls.freed_obj = 0
+
+    def __init__(self, shape, dtype):
+        self.shape = shape
+        self.dtype = dtype
+        self.unique_id = self.get_unique_id()
+
+    @classmethod
+    def empty_like(cls, other):
+        return cls(other.shape, other.dtype)
+
+    def method_return_self(self):
+        return self
+
+    def __iadd__(self, _):
+        return self
+
+    def __del__(self):
+        self.bump_freed()
+
+    @property
+    def self(self):
+        return self
+
+
+class TestArrayPool(TestCase):
+    def test_proxy(self):
+        def helper(wrapper, expected):
+            def type_assertion(i):
+                self.assertIsInstance(a, MockArray)
+                if expected[i]:
+                    self.assertIsInstance(a, ManagedObj)
+                else:
+                    self.assertNotIsInstance(a, ManagedObj)
+
+            array_pool = ManagedArrayPool(
+                arr_proto, MockArray.empty_like, unique_id=lambda arr: arr.unique_id,
+                method_wrapper=wrapper
+            )
+            # 0 - basic
+            a = array_pool.get()
+            type_assertion(0)
+            # 1 - i-operator
+            a += 1
+            type_assertion(1)
+            # 2 - explicit i-operator
+            a = array_pool.get().__iadd__(2)
+            type_assertion(2)
+            # 3 - dynamic callable attr
+            a = array_pool.get()
+            a.dynamic_return_self = lambda x: x
+            a = a.dynamic_return_self(a)
+            type_assertion(3)
+            # 4 - bound method
+            a = array_pool.get().method_return_self().method_return_self().method_return_self()
+            type_assertion(4)
+            # 5 - getter property
+            a = array_pool.get().self
+            type_assertion(5)
+            # 6 - class method (should NOT be proxied)
+            a = array_pool.get()
+            a = a.empty_like(a)
+            type_assertion(6)
+
+        arr_proto = MockArray([1024, 1024], float)
+        helper(None, [True, True, True, True, False, False, False])
+        helper('chainable', [True, True, True, True, True, False, False])
+        helper('descriptor', [True, True, True, True, True, True, False])
+
+    def test_pool(self):
+        arr_proto = MockArray([1024, 1024], float)
+        MockArray.clear_counters()
+        array_pool = ManagedArrayPool(arr_proto, MockArray.empty_like, unique_id=lambda arr: arr.unique_id,
+                                      method_wrapper='chainable')
+
+        # Test basic get
+        self.assertEqual(array_pool.allocated_count, 0)
+        a = array_pool.get()
+        self.assertEqual(array_pool.allocated_count, 1)
+        self.assertIsInstance(a, MockArray)
+        self.assertIsInstance(a, ManagedObj)
+
+        # Test basic recycle
+        arr_addr = id(a.__wrapped__)
+        self.assertEqual(len(array_pool._pool), 0)
+        a.dispose()
+        self.assertEqual(len(array_pool._pool), 1)
+        self.assertEqual(array_pool.allocated_count, 1)
+        for i in range(10):
+            del a
+            a = array_pool.get()
+        self.assertEqual(len(array_pool._pool), 0)
+        self.assertEqual(array_pool.allocated_count, 1)
+        self.assertEqual(id(a.__wrapped__), arr_addr)
+        self.assertIsInstance(a, MockArray)
+        self.assertIsInstance(a, ManagedObj)
+        a = []
+        a.append(array_pool.get())
+        self.assertEqual(id(a[0].__wrapped__), arr_addr)
+
+        # More recycle
+        del a
+        self.assertEqual(len(array_pool._pool), 1)
+        self.assertEqual(array_pool.allocated_count, 1)
+        self.assertEqual(MockArray.id_counter, 1)
+        self.assertEqual(MockArray.freed_obj, 0)
+        del array_pool
+        self.assertEqual(MockArray.freed_obj, 1)
+        array_pool = ManagedArrayPool(arr_proto, MockArray.empty_like, unique_id=lambda arr: arr.unique_id)
+        a = array_pool.get()
+        del array_pool
+        self.assertEqual(MockArray.id_counter, 2)
+        self.assertEqual(MockArray.freed_obj, 1)
+        del a
+        self.assertEqual(MockArray.freed_obj, 2)
